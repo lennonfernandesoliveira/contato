f40deb4c14222c23350df642bbe489bfaac3350f
lennonfernandesoliveira <lennonoliveira@gmail.com>
2020-09-25 18:32:43 -0300

    Adicionando ligação.

--- lib/components/userTile.dart
+++ lib/components/userTile.dart
@@ -43,27 +43,8 @@ class UserTile extends StatelessWidget {
                 icon: Icon(Icons.delete),
                 color: Colors.red,
                 onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (ctx) => AlertDialog(
-                            title: Text('Excluir contato'),
-                            content: Text('Tem certeza?'),
-                            actions: <Widget>[
-                              FlatButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text('Não')),
-                              FlatButton(
-                                  onPressed: () {
-                                    Provider.of<ProviderUsers>(context,
-                                            listen: false)
-                                        .remove(usuario);
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text('Sim'))
-                            ],
-                          ));
+                  Provider.of<ProviderUsers>(context, listen: false)
+                      .remove(usuario);
                 },
               ),
               IconButton(
